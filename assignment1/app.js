d88472090a9a3a0b2ac4d8668587daeb07a567f0
deepesh <deepeshnith547@gmail.com>
2016-09-20 11:54:36 -0700

    assignment 1 completed

---
+++ assignment1/app.js
@@ -0,0 +1,37 @@
+(function () {
+'use strict';
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', lunchchecker);
+lunchchecker.$inject = ['$scope'];
+
+function lunchchecker($scope) {
+    $scope.items = "";
+    $scope.message = "";
+    $scope.isValid = "";
+    $scope.check = function(){
+    var items = $scope.items;
+    if (items === '') {
+      $scope.message = "Please enter data first";
+      $scope.isValid = false;
+    } else {
+      items = items.split(',');
+      $scope.isValid = true;
+      var totalCount = 0;
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i].trim();
+        if (item !== "") {
+            totalCount += 1
+        }
+      }
+      if(totalCount <= 3) {
+        $scope.message = "Enjoy!";
+      }else{
+        $scope.message = "Too much!";
+      }
+    }
+
+  }
+
+}
+
+})();
